9eb67f7da09c6991bee9f68e3d54450afdbb42df
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2018-07-25 16:25:03 +0200

    new de haan data

--- import_scripts/import_dehaan.py
+++ import_scripts/import_dehaan.py
@@ -11,7 +11,7 @@ sys.path.append(os.path.dirname(__file__))
 from common import Importer
 from common import to_date
 
-SOURCE = '/media/data2/projects/dasa/data/dehaan/CollDeHaan_inventory_V20180517_2.xlsx'
+SOURCE = '/media/data2/projects/dasa/data/dehaan/CollDeHaan_inventory_V20180725.xlsx'
 
 
 class PlacardImporter(Importer):
