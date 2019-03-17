7999f3a1d0f99d14846456c5ea52e72872e0560d
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2019-03-17 09:21:09 +0100

    new corpusdiplomaticum data

--- import_scripts/import_corpusdiplomaticum.py
+++ import_scripts/import_corpusdiplomaticum.py
@@ -14,7 +14,7 @@ from common import Importer
 DATA_DIR = os.path.abspath(os.path.join(THIS_DIR, '..', '..', 'data'))
 PERSONEN_SOURCE_FN = os.path.join(DATA_DIR, 'corpusdiplomaticum', 'CD_vol1_personen_20170306_2.xlsx')
 PLAATSEN_SOURCE_FN = os.path.join(DATA_DIR, 'corpusdiplomaticum', 'CD_vol1_plaatsen_20170306_3.xlsx')
-CORPUS_SOURCE_FN =   os.path.join(DATA_DIR, 'corpusdiplomaticum', 'DASA_CorpsDipl_V20170927_vol1.xlsx')
+CORPUS_SOURCE_FN =   os.path.join(DATA_DIR, 'corpusdiplomaticum', 'DASA_CorpsDiplFull_V20190226.xlsx')
 
 
 class PersonenImporter(Importer):
